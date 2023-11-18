9a93860b11dfe144da9a11183ea68a07f06cb5f9
faltrum <hnsfairplay@gmail.com>
2023-11-18 22:41:58 +0100

    debugging

--- src/checker.c
+++ src/checker.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   checker.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mcatalan@student.42barcelona.com <mcata    +#+  +:+       +#+        */
+/*   By: osg <osg@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 14:19:41 by oseivane          #+#    #+#             */
-/*   Updated: 2023/11/17 12:04:21 by mcatalan@st      ###   ########.fr       */
+/*   Updated: 2023/11/18 22:40:37 by osg              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,7 +93,7 @@ void	checker(t_game *game)
 
 	init_and_use_map2d(game, &start_row, &start_col);
 	find_path(game, start_row, start_col);
-	if (game->exit && game->item == game->max_gift)
+	if (game->exit && game->gift == game->max_gift)
 	{
 		ft_printf("\n");
 		ft_free_map2d(game);
